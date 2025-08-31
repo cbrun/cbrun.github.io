2aed9012936d7a427dfe7644a6e43dedbcd8647b
cbrun <cedric.brun@obeo.fr>
2025-08-31 18:13:34 +0200

    update favicon

--- favicon.sh
+++ favicon.sh
@@ -1,14 +1,12 @@
-# PNG classiques pour favicon et PWA
-convert -background none favicon_cb_dark.svg -resize 32x32 favicon-32.png
-convert -background none favicon_cb_dark.svg -resize 192x192 favicon-192.png
-convert -background none favicon_cb_dark.svg -resize 512x512 favicon-512.png
+# Favicon PNGs
+magick favicon_cb_dark.svg -background none -alpha on -resize 32x32   PNG32:favicon-32.png
+magick favicon_cb_dark.svg -background none -alpha on -resize 64x64   PNG32:favicon-64.png
+magick favicon_cb_dark.svg -background none -alpha on -resize 96x96   PNG32:favicon-96.png
+magick favicon_cb_dark.svg -background none -alpha on -resize 192x192 PNG32:favicon-192.png
+magick favicon_cb_dark.svg -background none -alpha on -resize 512x512 PNG32:favicon-512.png
 
 # Apple touch icon
-convert -background none favicon_cb_dark.svg -resize 180x180 apple-touch-icon.png
+magick favicon_cb_dark.svg -background none -alpha on -resize 180x180 PNG32:apple-touch-icon.png
 
-convert -background none favicon_cb_dark.svg -define icon:auto-resize=16,32,48 favicon.ico
-
-
-convert -background none favicon_cb_dark.svg -resize 64x64 favicon-64.png
-convert -background none favicon_cb_dark.svg -resize 96x96 favicon-96.png
-
+# ICO with multiple sizes (transparency preserved)
+magick favicon_cb_dark.svg -background none -alpha on -define icon:auto-resize=16,32,48 favicon.ico
